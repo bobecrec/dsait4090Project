2d0e6e6cc760a1c7bc0aac0dd783699e54be43cd
bmarinov <b.k.marinov-1@student.tudelft.nl>
2025-12-04 12:54:02 +0100

    add gitignore for data and main for loading 1200 dev test set for performing the experiments

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+with open("data/dev.json") as f:
+    dev = json.load(f)
+
+# Take first 1200 examples
+subset = dev[:1200]
+
+# Save to new file
+with open("data/1200devTestSet.json", "w") as f:
+    json.dump(subset, f, indent=4)
+
+print("Saved 1200 entries to 1200devTestSet.json")
